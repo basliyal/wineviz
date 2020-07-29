c3de0bb5fdb3d2313c2927689be122ba205b6ecc
basliyal <basliyal83@gmail.com>
2020-07-28 21:41:29 -0400

    First Page Complete

--- winerate.js
+++ winerate.js
@@ -1,87 +1,148 @@
-var margin = {top: 10, right: 30, bottom: 30, left: 70},
-    width = 460 - margin.left - margin.right,
-    height = 400 - margin.top - margin.bottom;
+// first promise returns the dataset
+const dataset = d3.csv("https://raw.githubusercontent.com/basliyal/wineviz/master/winemag-data_first150k.csv")
+.then(function(data){
+  return data;
+});
+//this promise returns an array with lattitude and longitude
+const values = dataset.then(function(value) {
+  return Promise.all(value.map(function(results){
+    return [results.points, results.price];
+}))});
+  //print the array
+values.then(function(value) {
+  drawChart("#ratingcurve",value.map(function(results){
+    return parseInt(results[0]);
+  }), 88, "Wine Rating Curve", "Wine Points")});
 
-var svg = d3.select("#ratingcurve")
-  .append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
+values.then(function(value) {
+    drawChart("#pricecurve",value.map(function(results){
+      return parseInt(results[1]);
+    }), 40, "Wine Price chart", "Price per bottle")});
 
 
-d3.csv("https://raw.githubusercontent.com/basliyal/wineviz/master/winemag-data_first150k.csv", function(data){
-  // X axis: scale and draw:
-var x = d3.scaleLinear()
-    .domain([70, 100])     // can use this instead of 1000 to have the max of data: d3.max(data, function(d) { return +d.price })
+function drawChart(dom, data,thresold, main, xax) {
+  // set the dimensions and margins of the graph
+  var margin = {top: 30, right: 50, bottom: 70, left: 70},
+      width = 500 - margin.left - margin.right,
+      height = 500 - margin.top - margin.bottom;
+
+
+
+    var data=data.filter((d)=>{return !isNaN(d)})
+    .filter(function(d) { return d < 250 })
+
+    var svg = d3.select(dom)
+    .append("svg")
+      .attr("width", width + margin.left + margin.right)
+      .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+      .attr("transform",
+            "translate(" +  margin.left + "," + margin.top + ")");
+
+
+// X axis
+
+    var x = d3.scaleLinear()
+    .domain([d3.min(data), d3.max(data)])
     .range([0, width]);
-
-svg.append("g")
+    svg.append("g")
     .attr("transform", "translate(0," + height + ")")
     .call(d3.axisBottom(x));
 
     // text label for the x axis
     svg.append("text")
-        .attr("transform",
-              "translate(" + (width/2) + " ," +
-                             (height + margin.top + 20) + ")")
-        .style("text-anchor", "middle")
-        .text("Wine Points");
+    .attr("transform", "translate(" + (width / 2) + " ," + (height + margin.top) + ")")
+    .style("text-anchor", "middle")
+    .text(xax);
 
-// set the parameters for the histogram
-var histogram = d3.histogram()
-    .value(function(d) { return d.points; })
+    // set the parameters for the histogram
+    var histogram = d3.histogram()
+    .value(function(d) { return d })
     .domain(x.domain())
     .thresholds(x.ticks(42));
-var bins = histogram(data);
+    var bins = histogram(data);
+    console.log(bins);
 
-// Y axis: scale and draw:
-var y = d3.scaleLinear()
-    .range([height, 0]);
-    y.domain([0, d3.max(bins, function(d) { return d.length; })]);
-svg.append("g")
+    // Y axis: scale and draw:
+    var y = d3.scaleLinear()
+    .range([height, 0])
+    .domain([0, d3.max(bins, function(d) { return d.length;})]);
+
+    svg.append("g")
     .call(d3.axisLeft(y));
 
     // text label for the y axis
     svg.append("text")
-        .attr("transform", "rotate(-90)")
-        .attr("y", 0 - margin.left)
-        .attr("x",0 - (height / 2))
-        .attr("dy", "1em")
-        .style("text-anchor", "middle")
-        .style("font-size", "18px")
-        .text("# of Wines");
+    .attr("transform", "rotate(-90)")
+    .attr("y", 0 - margin.right)
+    .attr("x",0 - (height / 2))
+    .style("text-anchor", "middle")
+    .style("font-size", "18px")
+    .text("# of Wines");
 
-svg.selectAll("rect")
+    svg.selectAll("rect")
     .data(bins)
     .enter()
     .append("rect")
-      .attr("x", 1)
-      .attr("transform", function(d) { return "translate(" + x(d.x0) + "," + y(d.length) + ")"; })
-      .attr("width", function(d) { return x(d.x1) - x(d.x0) -1 ; })
-      .attr("height", function(d) { return height - y(d.length); })
-      .style("fill", function(d){ if(d.x0<85){return "orange"} else {return "teal"}});
+    .attr("transform", function(d) { return "translate(" + x(d.x0) + "," + y(d.length) + ")";})
+    .attr("x", 0)
+    .attr("y", y(0))
+    .attr("width", function(d) { return x(d.x1) - x(d.x0) ; })
+    .attr("height",height)
+    .transition()
+    .duration(800)
+    .attr("y", function(d) { return y(0) - height })
+    .attr("height", function(d) { return height - y(d.length); })
 
-svg.append("line")
-    .attr("x1", x(10) )
-    .attr("x2", x(10) )
-    .attr("y1", y(0))
-    .attr("y2", y(30000))
-    .attr("stroke", "grey")
-    .attr("stroke-dasharray", "4")
+    .delay(function(d,i){return(i*50)})
+    .style("fill", function(d){ if(d.x0<thresold){return "orange"} else {return "teal"}})
 
-svg.append("text")
-  .attr("x", x(10))
-  .attr("y", y(15000))
-  .text("Threshold: 6.5 Quality rating")
-  .style("font-size", "15px")
-
-  svg.append("text")
-          .attr("x", (width - 50))
-          .attr("y", 20 - (margin.top / 2))
-          .attr("text-anchor", "middle")
-          .style("font-size", "18px")
-          .text("Wine Rating Curve");
-
-svg.selectAll("g.tick text")
-  .style("fill","green");
-
+    svg.selectAll("rect")
+    .on("mouseover", function() { tooltip.style("display", null);
+    d3.select(this).style("fill", "red");})
+.on("mouseout", function() { tooltip.style("display", "none");
+d3.select(this).style("fill", function(d) {
+               if(d.x0<thresold){return "orange"}
+               else {return "teal"}
+           }); })
+.on("mousemove", function(d) {
+  tooltip.attr("transform", "translate(" + x(d.x0) + ", " + d3.event.y/3 + ")")
+  tooltip.select("text").text("Wine Count: "+d.length);
 });
+
+
+    svg.append("text")
+    .append('svg:tspan')
+    .attr("x", (width - margin.right ))
+    .attr("text-anchor", "middle")
+    .style("font-size", "18px")
+    .html(main)
+    .append('svg:tspan')
+    .attr("x", (width - margin.right ))
+    .attr('dy', 20)
+    .attr("text-anchor", "middle")
+    .style("font-size", "18px")
+    .html("Thresold Indicator:"+thresold);
+
+
+    svg.selectAll("g.tick text")
+    .style("fill","green");
+
+
+    // Prep the tooltip bits, initial display is hidden
+var tooltip = svg.append("g")
+  .attr("class", "tooltip")
+  .style("display", "none");
+
+tooltip.append("rect")
+  .attr("width", 160)
+  .attr("height", 20)
+  .attr("fill", "yellow")
+
+tooltip.append("text")
+  .attr("x", 80)
+  .attr("dy", "1.2em")
+  .style("text-anchor", "middle")
+  .attr("font-size", "15px")
+  .attr("font-weight", "bold");
+  }
--- worldmap.js
+++ worldmap.js
@@ -1,20 +1,43 @@
 var margin = {top: 10, right: 30, bottom: 30, left: 70},
-    width = 460 - margin.left - margin.right,
+    width = 740 - margin.left - margin.right,
     height = 400 - margin.top - margin.bottom;
 
-
-var projection = d3.geoMercator()
-.scale(width / 3 / Math.PI)
-      //.scale(100)
-      .translate([width / 3, height / 3])
-
 var svg1 = d3.select("#countryrate").append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+    .attr("transform",
+          "translate(" + margin.left + "," + margin.top + ")");
 
-var path = d3.geoPath()
- .projection(projection);
+var path = d3.geoPath();
+var projection = d3.geoMercator()
+      .scale(70)
+      .center([0,20])
+      .translate([width / 2, height / 2]);
 
- var url = "http://enjalot.github.io/wwsd/data/world/world-110m.geojson";
- d3.json(url, function(err, geojson) {
-   svg1.append("path")
-     .attr("d", path(geojson))
-});
+var data = d3.map();
+var colorScale = d3.scaleThreshold()
+      .domain([100000, 1000000, 10000000, 30000000, 100000000, 500000000])
+      .range(d3.schemeBlues[7]);
+      var promises = [];
+
+      promises.push(d3.json("https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/world.geojson"))
+      promises.push(d3.csv("https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/world_population.csv"))
+
+      Promise.all(promises).function(d) {
+        data.set(d.code, +d.pop)
+        svg1.append("g")
+          .selectAll("path")
+          .data(topo.features)
+          .enter()
+          .append("path")
+            // draw each country
+            .attr("d", d3.geoPath()
+              .projection(projection)
+            )
+            // set the color of each country
+            .attr("fill", function (d) {
+              d.total = data.get(d.id) || 0;
+              return colorScale(d.total);
+            });
+          }
