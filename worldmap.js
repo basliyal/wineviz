0bb14dcc63e0ec17ebadb9d66427248a32157abe
basliyal <basliyal83@gmail.com>
2020-07-31 16:01:56 -0400

    fixed csv

--- worldmap.js
+++ worldmap.js
@@ -1,7 +1,54 @@
 
+var val = JSON.parse(window.localStorage.getItem('value'));
+var aggdata = d3.nest()
+.key(function(d) { return d[2]; })
+.rollup(function(v) { return v.length })
+.entries(val)
+.reduce(function(map, obj) {
+    map[obj.key] = obj.val;
+    return map;
+}, {});
+
+var qualityavg = d3.nest()
+.key(function(d) { return d[2]; })
+.rollup(function(v) {
+  var count = v.length;
+  var total = d3.sum(v, function(d) { return d[0] })
+  return total/count;
+})
+.entries(val)
+.sort(function(a,b) {return d3.descending(a.value,b.value);})
+
+var priceavg = d3.nest()
+.key(function(d) { return d[2]; })
+.rollup(function(v) {
+  var count = v.length;
+  var total = d3.sum(v, function(d) { return d[1] })
+  return total/count;
+})
+.entries(val)
+.sort(function(a,b) {return d3.descending(a.value,b.value);})
+
+
+
+function drawtotal(){
+var aggdata = d3.nest()
+.key(function(d) { return d[2]; })
+.rollup(function(v) { return v.length })
+.entries(val)
+.reduce(function(map, obj) {
+    map[obj.key] = obj.value;
+    return map;
+}, {});
+drawChart(aggdata, "Total Rating Counts by Country");
+}
+
+
+
 function drawChart(data){
 var width = 1200,
     height = 700;
+
 
 var projection = d3.geoMercator()
     .center([10, 50]) //long and lat starting position
@@ -15,6 +62,14 @@ var svg = d3.select("body").append("svg")
 var path = d3.geoPath()
     .projection(projection);
 
+    var keys = Object.keys(data);
+    var values = keys.map(function(v) { return data[v]; });
+    console.log(values);
+var color =  d3.scaleLinear()
+  .domain([0,d3.mean(values), d3.max(values)])
+  .range(['yellow', 'green', 'blue']);
+
+
 var g = svg.append("g");
 
 // load and display the world and locations
@@ -24,7 +79,10 @@ var world = g.selectAll("path")
 				.enter()
 				.append("path")
 				.attr("d", path)
-        .on('mouseover', function(d){
-});
+        .attr("fill", function(d) { val = data[d.properties.name] ||0;
+        return color(val);})
+        .on('mouseover', function (d) {
+          console.log(data) ;
+             console.log(data[d.properties.name])});
 });
 }
